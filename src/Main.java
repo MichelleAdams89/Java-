97f0e174df3457da5d4eed50645441ed4c412418
MichelleAdams89 <efuamichelle18@gmail.com>
2025-01-24 00:58:06 +0000

    Body

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,51 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        //STEPS IN CREATING A VARIABLE
+        // 1.Declaration
+        //2. Assignment
+        int age =30;
+        int year=2025;
+        System.out.println("I am " + age);
+        System.out.println( "The year is " +2025 );
+
+        //DOUBLE
+        double price= 19;
+        double gpa =3.6;
+        double temperature =-12.1;
+        System.out.println( "$ " + price);
+
+        // char
+        char grade = 'A';
+        char symbol='!';
+        char currency='$';
+        System.out.println("I had a grade "+ grade);
+        System.out.println("The currency in USA is "+  currency);
+        System.out.println("I told Michelle to stop sleeping always ah" +  symbol);
+
+        //Booleans are True or FALSE ;
+        boolean isStudent = true;
+        boolean forSale =true ;
+        boolean isOnline =true;
+        System.out.println(isStudent);
+        //IF AND ELSE STATEMENT IN BOOLEAN
+        //STRINGS are series of characters
+
+        String food= "Pizza";
+        System.out.println(food);
+        String name ="Michelle";
+        String email= "Michelleasa12a@gmail.com";
+        String car = "Jeep";
+        String color = "Black";
+        System.out.println("My email is  " + email);
+        System.out.println( "My name is " + name + " and my favourite food is "+ food);
+        System.out.println("Hello " + name);
+        System.out.println(" You are " + age);
+        System.out.println(" You gpa is : " + gpa);
+        System.out.println( " Your average letter grade is:" + grade);
+
+        System.out.println( "My choice is a " + color + " "+  year +" " + car);
+        System.out.println( "The price is:" + currency + price );
+
+
     }
 }
